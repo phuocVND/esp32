c2103413a9cd8789673afb953fd10948ddf3c8cd
phuocVND <vonguyenduyphuoc2001@gmail.com>
2025-05-17 13:05:03 +0700

    Update tcp_client.h

--- project-tcp-client/main/tcp_client.h
+++ project-tcp-client/main/tcp_client.h
@@ -4,22 +4,23 @@
 #include <stdbool.h>
 // Định nghĩa TAG và hằng số (thay vì dùng ESP-IDF)
 #define TCP_TAG "TCP_CLIENT"
-#define TCP_SERVER_IP "192.168.1.213" // Thay bằng IP server của bạn
+#define TCP_SERVER_IP "192.168.1.54" // Thay bằng IP server của bạn
 #define TCP_SERVER_PORT 8080
-#define NUM_INTS 5
+#define NUM_INTS 6
 typedef struct {
     int id;
     float value;
     char name[32];
 } MyData;
 
-void send_array(int client_socket, float* arr, size_t size);
-bool recv_array(int client_socket, int* arr, size_t size);
+void send_array(int client_socket, short* arr, size_t size);
+bool recv_array(int client_socket, short* arr, size_t size);
 void send_struct(int client_socket, const MyData* data);
 bool recv_struct(int client_socket, MyData* data);
 int tcp_client_init(void);
 int tcp_client_send(const char *data);
-int tcp_client_receive_ints(int *int_array);
+short tcp_client_receive_ints(short *int_array);
+short tcp_client_send_ints(short *int_array);
 int tcp_client_receive(char *buffer, size_t buffer_size);
 void tcp_client_close(void);
 
